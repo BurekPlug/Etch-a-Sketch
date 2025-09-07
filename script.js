16bfc5121ef74ff9b2ba3b1a1582a0337efcf664
BurekPlug <198450603+BurekPlug@users.noreply.github.com>
2025-09-07 19:56:32 +0200

    added hover effect

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const NUMBEROFROWS = 16;
 //create one Row:
 const ColDiv = document.createElement("div");
 ColDiv.classList.add("ColDiv");
-ColDiv.textContent = "Testcontent"
 //add to an empty html or get the reference to the whole html
 document.documentElement.appendChild(ColDiv);
 ColDiv.setAttribute("class", "ColContainer");
@@ -26,18 +25,10 @@ for (let i = 0; i < NUMBEROFROWS; i++)
     }
 }
 
-/*//create one Row:
-const RowDiv = document.createElement("div");
-RowDiv.classList.add("RowDiv");
-RowDiv.textContent = "Testcontent"
-RowDiv.setAttribute("class", "RowContainer");
-ColDiv.appendChild(RowDiv);
 
-// Create boxes in Rows
-for (let i = 0; i < NUMBEROFROWS; i++) 
-{
-    const box = document.createElement("div");
-    box.setAttribute("class", "Box");
-    RowDiv.appendChild(box);
-}
-*/
+const Boxes = document.querySelectorAll(".Box")
+
+Boxes.forEach( (boxRef) => {
+    boxRef.addEventListener("mouseover", () => boxRef.setAttribute("class", "hoverBox"));
+});
+
