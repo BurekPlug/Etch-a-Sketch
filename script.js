41df9dd52c5cb64cefdbe547202521c73a45b445
BurekPlug <198450603+BurekPlug@users.noreply.github.com>
2025-09-07 20:43:51 +0200

    added New button for generating new grid

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
 const NUMBEROFROWS = 16;
 
+const gridBtn = document.querySelector("#gridButton");
+gridBtn.addEventListener("click", promptFunction);
 
-//Create Columcontainer, in which the Rows will be enlisted.
-//create one Row:
-const ColDiv = document.createElement("div");
-ColDiv.classList.add("ColDiv");
-//add to an empty html or get the reference to the whole html
-document.documentElement.appendChild(ColDiv);
-ColDiv.setAttribute("class", "ColContainer");
-
-for (let i = 0; i < NUMBEROFROWS; i++) 
-{
-    const RowDiv = document.createElement("div");
-    RowDiv.classList.add("RowDiv");
-    RowDiv.setAttribute("class", "RowContainer");
-    ColDiv.appendChild(RowDiv);
-
-    // Create boxes in Rows
-    for (let i = 0; i < NUMBEROFROWS; i++) 
-    {
-        const box = document.createElement("div");
-        box.setAttribute("class", "Box");
-        RowDiv.appendChild(box);
-    }
+function promptFunction() {
+    let nmbRows = prompt("How big shall the grid be?", "0");
+    console.log(nmbRows);
+    deleteGrid();
+    if (nmbRows <= 100) {generateGrid(nmbRows);}
+    
 }
 
+function generateGrid (numberOfRows)
+{
+    //Create Columcontainer, in which the Rows will be enlisted.
+    //create one Row:
+    const ColDiv = document.createElement("div");
+    ColDiv.classList.add("ColDiv");
+    //add to an empty html or get the reference to the whole html
+    document.documentElement.appendChild(ColDiv);
+    ColDiv.setAttribute("class", "ColContainer");
 
-const Boxes = document.querySelectorAll(".Box")
+    for (let i = 0; i < numberOfRows; i++) 
+    {
+        const RowDiv = document.createElement("div");
+        RowDiv.classList.add("RowDiv");
+        RowDiv.setAttribute("class", "RowContainer");
+        ColDiv.appendChild(RowDiv);
 
-Boxes.forEach( (boxRef) => {
-    boxRef.addEventListener("mouseover", () => boxRef.setAttribute("class", "hoverBox"));
-});
+        // Create boxes in Rows
+        for (let i = 0; i < numberOfRows; i++) 
+        {
+            const box = document.createElement("div");
+            box.setAttribute("class", "Box");
+            RowDiv.appendChild(box);
+        }
+    }
 
+
+    const Boxes = document.querySelectorAll(".Box")
+
+    Boxes.forEach( (boxRef) => {
+        boxRef.addEventListener("mouseover", () => boxRef.setAttribute("class", "hoverBox"));
+    });
+
+}
+
+function deleteGrid () {
+    const allGridElem = document.querySelectorAll("div");
+    allGridElem.forEach((e) => e.remove());
+}
